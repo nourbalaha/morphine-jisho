9ee91d8564a63f951b13de30e2ef19ef711fc209
Nour Balaha <nourbalaha0@gmail.com>
2020-07-10 17:37:40 +0900

    created initial elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <h1>Medical Dictionary</h1>
       </header>
-      <main>
-        <div className="side-bar"></div>
-        <div className="result-page"></div>
+      <main className="main">
+        <div className="side-bar">
+          <div className="search-bar">
+            <input className="search-input" type="text" placeholder="Search" />
+            <button className="search-btn">Go</button>
+          </div>
+          <div className="word-list">
+            <span className="word">Hi</span>
+            <span className="word">Hello</span>
+            <span className="word">Here</span>
+            <span className="word">Him</span>
+            <span className="word">Her</span>
+            <span className="word">His</span>
+            <span className="word">Hi</span>
+            <span className="word">Hello</span>
+            <span className="word">Here</span>
+            <span className="word">Him</span>
+            <span className="word">Her</span>
+            <span className="word">His</span>
+            <span className="word">Hi</span>
+            <span className="word">Hello</span>
+            <span className="word">Here</span>
+            <span className="word">Him</span>
+            <span className="word">Her</span>
+            <span className="word">His</span>
+            <span className="word">Hi</span>
+            <span className="word">Hello</span>
+            <span className="word">Here</span>
+            <span className="word">Him</span>
+            <span className="word">Her</span>
+            <span className="word">His</span>
+          </div>
+        </div>
+        <div className="result-page">
+          <h2>Hi</h2>
+          <p>Used when greting somone</p>
+          <hr />
+          <h2>examples</h2>
+          <h3>example #1</h3>
+          <p>Hi every one.</p>
+          <h3>example #2</h3>
+          <p>Hi there, my name is Nour</p>
+        </div>
       </main>
     </div>
   );
